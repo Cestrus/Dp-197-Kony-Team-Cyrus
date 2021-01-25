e0bbf38915d92b6aedf45437a95d26b91befab21
Daryna Hunko <dhun@softserveinc.com>
2021-01-25 21:24:13 +0200

    added start and main screens, added navigation func to the main screen, added nav bar and header

---
+++ controllers/mobile/frmStartController/frmStartController.js
@@ -0,0 +1,17 @@
+define({ 
+  onInitialize: function() {
+    this.view.btnStart.onClick = this.onButtonEnterClicked.bind(this);
+  },
+  
+  onButtonEnterClicked: function() {
+    if (this.view.inptStart.text.toLocaleLowerCase() === "ready for the space") {
+      var navigation = new kony.mvc.Navigation("frmMain");
+      navigation.navigate();
+    } else {
+      this.view.inptStart.setFocus(true);
+      this.view.inptStart.placeholder = "Enter the password here again";
+    }
+  }
+  
+
+ });
