209fcbe65d1a9a3818e4da262b0d7af2c68979e9
Daryna Hunko <dhun@softserveinc.com>
2021-01-27 19:18:32 +0200

    added news screen, changed a little bit the navigation menus

--- controllers/mobile/frmMainController/frmMainController.js
+++ controllers/mobile/frmMainController/frmMainController.js
@@ -1,16 +1,26 @@
 define({ 
   onInitialize: function() {
-    this.view.btnGoToWeather.onClick = this.onButtonGoToWeather.bind(this);
-    this.viewbtnGoToSearchPhotos.onClick = this.onButtonGoToSearchPhotos.bind(this);
+    //this.view.btnGoToWeather.onClick = this.onButtonGoToWeather.bind(this);
+    //this.view.tabBtnWeather.onClick = this.onButtonGoToWeather.bind(this);
+    //this.view.btnGoToSearchPhotos.onClick = this.onButtonGoToSearchPhotos.bind(this);
+    //this.view.tabBtnSearchImg.onClick = this.onButtonGoToSearchPhotos.bind(this);
+	this.view.btnGoToNews.onClick = this.onButtonGoToNews.bind(this);
+	this.view.tabBtnNews.onClick = this.onButtonGoToNews.bind(this);
+
   },
   
   onButtonGoToWeather: function() {
-      var navigation = new kony.mvc.Navigation("frmID");
+      var navigation = new kony.mvc.Navigation("frmNews");
       navigation.navigate();
   },
   
   onButtonGoToSearchPhotos: function() {
-      var navigation = new kony.mvc.Navigation("frmID");
+      var navigation = new kony.mvc.Navigation("frmNews");
+      navigation.navigate();
+  },
+  
+  onButtonGoToNews: function() {
+      var navigation = new kony.mvc.Navigation("frmNews");
       navigation.navigate();
   }
 
