3375f2ee06586fc28e9e6db0e582e246da57fc60
Daryna Hunko <dhun@softserveinc.com>
2021-01-29 18:49:24 +0200

    added news list

--- controllers/mobile/frmNewsController/frmNewsController.js
+++ controllers/mobile/frmNewsController/frmNewsController.js
@@ -1,9 +1,53 @@
 define({ 
 onInitialize: function() {
+	this.view.postShow = this.onFormShowed.bind(this);
     //this.view.tabBtnWeather.onClick = this.onButtonGoToWeather.bind(this);
     //this.view.tabBtnSearchImg.onClick = this.onButtonGoToSearchPhotos.bind(this);
 	this.view.tabBtnHome.onClick = this.onButtonGoToHome.bind(this);
-	this.view.btnGoBack.onClick = this.onButtonGoToHome.bind(this);
+	this.view.btnGoBack.onClick = function () {
+      var navigation = new kony.mvc.Navigation(kony.application.getPreviousForm().id);
+      navigation.navigate();
+    }.bind(this);
+
+  },
+  
+  onFormShowed: function() {
+    this.loadNewsList();
+  },
+  
+  loadNewsList: function() {
+    kony.application.showLoadingScreen();
+    
+    // Creation phase
+    var httpClient = new kony.net.HttpRequest();
+    // Setup phase
+    httpClient.open(constants.HTTP_METHOD_GET, "https://content.guardianapis.com/search?section=science&order-by=newest&show-elements=all&show-fields=all&q=space%2C%20technology&api-key=a9cd8943-4ed2-4441-b3d4-4cbf89189828");
+    httpClient.onReadyStateChange = this.onNewsListReceived.bind(this, httpClient);
+    
+    // Action
+    httpClient.send();
+  },
+  
+  onNewsListReceived: function(httpClient) {
+    kony.print("Movie List Retrieval State Change: " + httpClient.readyState);
+
+    if(httpClient.readyState !== constants.HTTP_READY_STATE_DONE) {
+      return;
+    }
+    
+      
+    var newsData = httpClient.response;
+    var listData = newsData.results.map(function(m) {
+      return {
+        lblNewsTitle: m.webTitle,
+        lblNewsDate: m.webPublicationDate,
+        lblNewsShortDesc: m.fields.trailText,
+        imgNews: m.fields.thumbnail
+      };
+    });
+
+    this.view.lstNews.setData(listData);
+    kony.application.dismissLoadingScreen();
 
   },
   
