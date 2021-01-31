2e9a64b6e2de648d5297efa544d57f65e02e4f43
Alexey Savchenko <wasxela@gmail.com>
2021-01-31 20:22:08 +0200

    add actions to nav bar on collections screen

--- controllers/mobile/frmCollectionImgController/frmCollectionImgController.js
+++ controllers/mobile/frmCollectionImgController/frmCollectionImgController.js
@@ -1,5 +1,35 @@
 define({ 
-
- //Type your controller code here 
+ onInitialize: function() {
+    this.view.tabBtnHome.onClick = this.onButtonGoToHome.bind(this);
+    this.view.tabBtnSearchImg.onClick = this.onButtonGoToSearchImg.bind(this);
+    this.view.tabBtnNews.onClick = this.onButtonGoToNews.bind(this);
+//     this.view.tabBtnWeather.onClick = this.onButtonGoToWeather.bind(this);
+//     this.view.tabBtnCollection.onClick = this.onButtonGoToCollection.bind(this);
+  },
+  
+  onButtonGoToHome: function() {
+    var navigation = new kony.mvc.Navigation("frmMain");
+    navigation.navigate();
+  },
+  
+  onButtonGoToSearchImg: function() {
+      var navigation = new kony.mvc.Navigation("frmSearchImg");
+      navigation.navigate();
+  },  
+  
+  onButtonGoToNews: function() {
+      var navigation = new kony.mvc.Navigation("frmNews");
+      navigation.navigate();
+  },
+  
+  onButtonGoToWeather: function() {
+      var navigation = new kony.mvc.Navigation("frmWeather");
+      navigation.navigate();
+  },
+  
+//   onButtonGoToCollection: function() {
+//   	  var navigation = new kony.mvc.Navigation("frmCollectionImg");
+//       navigation.navigate();
+//   }
 
  });
