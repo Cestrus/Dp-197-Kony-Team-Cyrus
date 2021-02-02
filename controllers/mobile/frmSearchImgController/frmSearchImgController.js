63bd54280932524361cb63684ee894402dc6bc2e
Alexey Savchenko <wasxela@gmail.com>
2021-02-02 22:11:42 +0200

    add btn-search

--- controllers/mobile/frmSearchImgController/frmSearchImgController.js
+++ controllers/mobile/frmSearchImgController/frmSearchImgController.js
@@ -1,26 +1,25 @@
 define({ 
 
 onInitialize: function() {
+	this.resetVisiblity();
+  
     this.view.tabBtnHome.onClick = this.onButtonGoToHome.bind(this);
-//     this.view.tabBtnSearchImg.onClick = this.onButtonGoToSearchImg.bind(this);
     this.view.tabBtnNews.onClick = this.onButtonGoToNews.bind(this);
     this.view.tabBtnWeather.onClick = this.onButtonGoToWeather.bind(this);
+  
 	this.view.btnGoBack.onClick = function () {
       var navigation = new kony.mvc.Navigation(kony.application.getPreviousForm().id);
       navigation.navigate();
     }.bind(this);
+ 
+	this.URL_MAIN = "https://images-api.nasa.gov/search?q=";
   },
   
   onButtonGoToHome: function() {
     var navigation = new kony.mvc.Navigation("frmMain");
     navigation.navigate();
   },
-  
-//   onButtonGoToSearchImg: function() {
-//       var navigation = new kony.mvc.Navigation("frmSearchImg");
-//       navigation.navigate();
-//   },  
-  
+   
   onButtonGoToNews: function() {
       var navigation = new kony.mvc.Navigation("frmNews");
       navigation.navigate();
@@ -29,6 +28,18 @@ onInitialize: function() {
   onButtonGoToWeather: function() {
       var navigation = new kony.mvc.Navigation("frmWeather");
       navigation.navigate();
-  }
+  },
+
+  resetVisiblity: function() {
+	this.view.imgRocket.visibility  = true;
+	this.view.lbNotFound.visibility = false;
+    this.view.lstImg.visibility = false;
+  },
+  
+  setFullUrl: function(str) {
+	return this.URL_MAIN + str + "&page=1";
+}
+
+  
 
  });
