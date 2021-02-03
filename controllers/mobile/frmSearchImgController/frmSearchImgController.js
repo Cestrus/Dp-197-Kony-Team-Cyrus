42df48bc068bb4112088377a28d5aceb2fcc4896
Alexey Savchenko <wasxela@gmail.com>
2021-02-03 19:18:25 +0200

    have done search img without modules

--- controllers/mobile/frmSearchImgController/frmSearchImgController.js
+++ controllers/mobile/frmSearchImgController/frmSearchImgController.js
@@ -6,6 +6,7 @@ onInitialize: function() {
     this.view.tabBtnHome.onClick = this.onButtonGoToHome.bind(this);
     this.view.tabBtnNews.onClick = this.onButtonGoToNews.bind(this);
     this.view.tabBtnWeather.onClick = this.onButtonGoToWeather.bind(this);
+    this.view.btnSearch.onClick = this.onSendRequest.bind(this);
   
 	this.view.btnGoBack.onClick = function () {
       var navigation = new kony.mvc.Navigation(kony.application.getPreviousForm().id);
@@ -13,6 +14,7 @@ onInitialize: function() {
     }.bind(this);
  
 	this.URL_MAIN = "https://images-api.nasa.gov/search?q=";
+   
   },
   
   onButtonGoToHome: function() {
@@ -30,16 +32,68 @@ onInitialize: function() {
       navigation.navigate();
   },
 
+  // visibility methods
+  
   resetVisiblity: function() {
-	this.view.imgRocket.visibility  = true;
-	this.view.lbNotFound.visibility = false;
-    this.view.lstImg.visibility = false;
+	this.view.imgRocket.isVisible = true;
+	this.view.lbNotFound.isVisible = false;
+    this.view.lstImg.isVisible = false;
+    this.view.btnAddToCollection.isVisible = false;
   },
   
-  setFullUrl: function(str) {
-	return this.URL_MAIN + str + "&page=1";
-}
+  renderNotFound: function() {
+    this.view.imgRocket.isVisible = true;
+	this.view.lbNotFound.isVisible = true;
+    this.view.lstImg.isVisible = false;
+    this.view.btnAddToCollection.isVisible = false;
+  },
+  
+  renderListImg: function() {
+    this.view.imgRocket.isVisible = false;
+	this.view.lbNotFound.isVisible = false;
+    this.view.lstImg.isVisible = true;
+    this.view.btnAddToCollection.isVisible = false;
+  },
+  
+  // request methods
+  onSendRequest: function() {
+    kony.application.showLoadingScreen();
+    
+    var fullURL = this.URL_MAIN + this.view.inptSearchImg.text.split(' ').join('%20') + "&page=1";
+    var httpClient = new kony.net.HttpRequest();
+    httpClient.open(constants.HTTP_METHOD_GET, fullURL);
+    httpClient.onReadyStateChange = this.onImgListReceived.bind(this, httpClient); 
+    
+    httpClient.send();
+    
+  },
 
+  onImgListReceived: function(httpClient){
+    if(httpClient.readyState !== constants.HTTP_READY_STATE_DONE) {
+      return;
+    }
+    var responseData = httpClient.response;
+    var imgLinks = responseData.collection.items.map(function(item) {
+      if(item.links) {
+        return { imgSpace: item.links[0].href };
+      } else {
+        return { imgSpace: "" };
+      }
+    });
+    kony.application.dismissLoadingScreen();
+	this.checkImgList(imgLinks);
+  },
+    
+  checkImgList: function(arrLinks) {
+    var imgLinks = arrLinks.filter(function(link) {
+      return link.imgSpace !== "";
+    });
+	if(!imgLinks.length) this.renderNotFound();
+    else {
+      this.view.lstImg.setData(imgLinks);
+      this.renderListImg();
+    }    
+  },
   
 
  });
