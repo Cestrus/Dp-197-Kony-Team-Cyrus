0d489f1f8bfa82e5e91db1738fe0c25bac0cb39a
Daryna Hunko <dhun@softserveinc.com>
2021-02-04 18:05:12 +0200

    fixed separate news screen

--- controllers/mobile/frmSeparateNewsController/frmSeparateNewsController.js
+++ controllers/mobile/frmSeparateNewsController/frmSeparateNewsController.js
@@ -1,14 +1,11 @@
 define(["NewsService"], function(newsService) {
   return { 
-	onInitialize: function() {
-      //this.view.postShow = this.onFormShowed.bind(this);  
+	onInitialize: function() { 
       this.view.tabBtnHome.onClick = this.onButtonGoToHome.bind(this);
       this.view.tabBtnSearchImg.onClick = this.onButtonGoToSearchImg.bind(this);
       this.view.tabBtnWeather.onClick = this.onButtonGoToWeather.bind(this);
-      this.view.btnGoBack.onClick = function () {
-        var navigation = new kony.mvc.Navigation(kony.application.getPreviousForm().id);
-        navigation.navigate();
-      }.bind(this);
+      this.view.tabBtnNews.onClick = this.onButtonGoToNews.bind(this);
+      this.view.btnGoBack.onClick = this.onButtonGoToNews.bind(this);
 
     },
 
