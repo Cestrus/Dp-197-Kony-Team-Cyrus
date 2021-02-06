607b9abdf9231ab5416c3a96679b0f5afe05b017
Alexey Savchenko <wasxela@gmail.com>
2021-02-06 11:31:55 +0200

    fix bugs movement (add service functional into controllers)

--- controllers/mobile/frmWeatherController/frmWeatherController.js
+++ controllers/mobile/frmWeatherController/frmWeatherController.js
@@ -1,32 +1,43 @@
-define({ 
-  onInitialize: function() {
-	//this.view.postShow = this.onFormShowed.bind(this);  
-    this.view.tabBtnHome.onClick = this.onButtonGoToHome.bind(this);
-    this.view.tabBtnSearchImg.onClick = this.onButtonGoToSearchImg.bind(this);
-//     this.view.tabBtnNews.onClick = this.onButtonGoToNews.bind(this);
-	this.view.btnGoBack.onClick = function () {
-      var navigation = new kony.mvc.Navigation(kony.application.getPreviousForm().id);
-      navigation.navigate();
-    }.bind(this);
+define(["NewsService"], function(newsService) { 
+  return {
+    onInitialize: function() {
+      //this.view.postShow = this.onFormShowed.bind(this);  
+      this.view.tabBtnHome.onClick = this.onButtonGoToHome.bind(this);
+      this.view.tabBtnSearchImg.onClick = this.onButtonGoToSearchImg.bind(this);
+      this.view.tabBtnNews.onClick = this.onButtonGoToNews.bind(this);
+      
+      this.view.btnGoBack.onClick = function () {
+        var navigation = new kony.mvc.Navigation(kony.application.getPreviousForm().id);
+        navigation.navigate();
+      }.bind(this);
+    },
 
-  },
-
-  onNavigate: function(data) {
+    onNavigate: function(data) {
       this.view.lstDates.setData(data);
-  },
-  
-  onButtonGoToHome: function() {
+    },
+
+    onButtonGoToHome: function() {
       var navigation = new kony.mvc.Navigation("frmMain");
       navigation.navigate();
-  },
-  
-//     onButtonGoToNews: function() {
-//       to update
-//     },  
-  
-  onButtonGoToSearchImg: function() {
+    },
+
+    onButtonGoToNews: function() {
+      newsService.getNews( 
+        function(arr) {
+          var navigation = new kony.mvc.Navigation("frmNews");
+          navigation.navigate(arr);
+        },
+        function() {
+          alert("Error while retrieving news list.");
+        }
+      );
+    },
+
+    onButtonGoToSearchImg: function() {
       var navigation = new kony.mvc.Navigation("frmSearchImg");
       navigation.navigate();
-  }
-  
- });
+    }
+
+  };
+
+});
