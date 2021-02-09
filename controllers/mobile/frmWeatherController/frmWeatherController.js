fa722e3b4b08362d5f910d3db3aba1e251c189d2
Olha Salatova <olga.sharpmind@gmail.com>
2021-02-09 12:45:21 +0200

    Feat: replaced text with icon on Weather Screen

--- controllers/mobile/frmWeatherController/frmWeatherController.js
+++ controllers/mobile/frmWeatherController/frmWeatherController.js
@@ -5,7 +5,7 @@ define(["NewsService"], function(newsService) {
       this.view.tabBtnSearchImg.onClick = this.onButtonGoToSearchImg.bind(this);
       this.view.tabBtnNews.onClick = this.onButtonGoToNews.bind(this);
       
-      this.view.btnInfo.onClick = this.onButtonGoInfo.bind(this);
+//       this.view.btnInfo.onClick = this.onButtonGoInfo.bind(this);
       this.view.btnProfile.onClick = this.onGoToProfile.bind(this);
     },
 
@@ -35,10 +35,10 @@ define(["NewsService"], function(newsService) {
       navigation.navigate();
     },
     
-    onButtonGoInfo : function() {
-      var navigation = new kony.mvc.Navigation("frmWeatherInfo");
-      navigation.navigate();
-    },
+//     onButtonGoInfo : function() {
+//       var navigation = new kony.mvc.Navigation("frmWeatherInfo");
+//       navigation.navigate();
+//     },
     
     onGoToProfile: function() {
       var navigation = new kony.mvc.Navigation("frmCollectionImg");
