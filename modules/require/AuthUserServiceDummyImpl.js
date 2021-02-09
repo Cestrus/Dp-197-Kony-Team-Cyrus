89ea8dbb9f7539f4ea1914486a3c05240cdf4d6c
Alexey Savchenko <wasxela@gmail.com>
2021-02-09 15:07:24 +0200

    delete goBack actions

--- modules/require/AuthUserServiceDummyImpl.js
+++ modules/require/AuthUserServiceDummyImpl.js
@@ -2,9 +2,7 @@ define(function () {
   var checkUser = function(login, password, successCB, errorCB) {
     var dummyUser = {login: "test", password: "test"};
     
-//     var users = [kony.store.getItem("users")];
-    var users = [];
-    
+    var users = [kony.store.getItem("users")];    
     users.push(dummyUser);
     var matchedUsers = null;
     var callback = null;
