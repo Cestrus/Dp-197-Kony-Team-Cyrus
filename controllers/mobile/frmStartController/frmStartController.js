b63947ecfefb453964cbf57c094027e4184ee5d5
Alexey Savchenko <wasxela@gmail.com>
2021-02-23 15:14:59 +0200

    removed comments

--- controllers/mobile/frmStartController/frmStartController.js
+++ controllers/mobile/frmStartController/frmStartController.js
@@ -5,28 +5,28 @@ define(["AuthUserService"], function(authUser) {
     },
 
     onButtonEnterClicked: function() {
-//       var validationResult = this.validateInput();
-//       if (!validationResult.validation) {
-//         alert (validationResult.validationMessage);
-//       } else {
-//         if (this.view.switchNewUser.selectedIndex === 1) {
-//           authUser.checkUser(this.view.inptStartLogin.text, this.view.inptStartPsswd.text, 
-//             function() {
-//               var navigation = new kony.mvc.Navigation("frmMain");
-//               navigation.navigate();
-//             },function() {
-//               alert("User is not registrated. Please registrate first");
-//             });
-//         } else {
-//           authUser.registerUser(this.view.inptStartLogin.text, this.view.inptStartPsswd.text, 
-//             function() {
+      var validationResult = this.validateInput();
+      if (!validationResult.validation) {
+        alert (validationResult.validationMessage);
+      } else {
+        if (this.view.switchNewUser.selectedIndex === 1) {
+          authUser.checkUser(this.view.inptStartLogin.text, this.view.inptStartPsswd.text, 
+            function() {
               var navigation = new kony.mvc.Navigation("frmMain");
               navigation.navigate();
-//             },function() {
-//               alert("User already exists. Please try another login");
-//             });
-//         }
-//       }
+            },function() {
+              alert("User is not registrated. Please registrate first");
+            });
+        } else {
+          authUser.registerUser(this.view.inptStartLogin.text, this.view.inptStartPsswd.text, 
+            function() {
+              var navigation = new kony.mvc.Navigation("frmMain");
+              navigation.navigate();
+            },function() {
+              alert("User already exists. Please try another login");
+            });
+        }
+      }
       
     },
     
