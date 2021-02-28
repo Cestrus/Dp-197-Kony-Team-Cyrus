2def4349fc100cfc0eb285340ad68eae6084e095
Daryna Hunko <dhun@softserveinc.com>
2021-03-01 00:31:21 +0200

    article favorits - has issues with removing the article

--- controllers/mobile/frmSeparateNewsController/frmSeparateNewsController.js
+++ controllers/mobile/frmSeparateNewsController/frmSeparateNewsController.js
@@ -1,23 +1,80 @@
 define(["NewsService"], function(newsService) {
   return { 
-	onInitialize: function() { 
+    onInitialize: function() { 
       this.view.tabBtnHome.onClick = this.onButtonGoToHome.bind(this);
       this.view.tabBtnSearchImg.onClick = this.onButtonGoToSearchImg.bind(this);
       this.view.tabBtnWeather.onClick = this.onButtonGoToWeather.bind(this);
       this.view.tabBtnNews.onClick = this.onButtonGoToNews.bind(this);
       this.view.btnGoBack.onClick = this.onButtonGoToNews.bind(this);
+      this.view.btnFavoriteArticle.onClick = this.onButtonFavoriteArticle.bind(this);
 
     },
 
     onNavigate: function(data) {
-		this.view.imgNews.src = data.imgNews;
-		this.view.lblNewsTitle.text = data.lblNewsTitle;
-        this.view.lblNewsText.text = data.bodyText;
+      this.view.imgNews.src = data.imgNews;
+      this.view.lblNewsTitle.text = data.lblNewsTitle;
+      this.view.lblNewsText.text = data.bodyText;
+      alert("On navigate check of the store " + this.checkSavedArticles(data.lblNewsTitle));
+      if (this.checkSavedArticles(data.lblNewsTitle)[0]) {
+        this.view.btnFavoriteArticle.skin = 'sknFavotiteArticleFocus';
+      } else {
+        this.view.btnFavoriteArticle.skin = 'sknFavotiteArticle';
+      }
+    },
+    
+    checkSavedArticles: function(title) {
+      var articleStore = JSON.parse(kony.store.getItem("savedArticles"));
+      var index;
+      if (articleStore !== null) { 
+        for (index = 0; index < articleStore.length; index++) {
+          if (articleStore[index].lblNewsTitle === title ) {
+            return [1, index];
+          }
+        }
+      }
+      return [0];
+
+    },
+    
+    updateAtricleStore: function(article, action, index) {
+      var articleStore = JSON.parse(kony.store.getItem("savedArticles"));
+      alert("article" + article.lblNewsTitle + "action" + action + "index" + index);
+      if (articleStore === null && action === 1) {
+        articleStore = []; 
+        articleStore.push(article);
+        kony.store.setItem("savedArticles", JSON.stringify(articleStore));
+      } else {
+        if (action === 1) {
+          articleStore.push(article);
+          kony.store.setItem("savedArticles", JSON.stringify(articleStore));
+        } else {
+          kony.store.setItem("savedArticles", JSON.stringify(articleStore.splice(index, 1)));
+        }
+      }
+      alert(articleStore);
+    },
+
+    onButtonFavoriteArticle: function() {
+      var article = {
+        //imgNews: this.view.imgNews.src,
+        lblNewsTitle: this.view.lblNewsTitle.text,
+        //lblNewsText: this.view.lblNewsText.text
+      };
+      var articleFinder = this.checkSavedArticles(article.lblNewsTitle);
+      alert("onButtonFavoriteArticle: " + articleFinder[0]);
+      if (articleFinder[0]) {
+        this.view.btnFavoriteArticle.skin = 'sknFavotiteArticle';
+        this.updateAtricleStore(article, 0, articleFinder[1]);
+      } else {
+        this.view.btnFavoriteArticle.skin = 'sknFavotiteArticleFocus';
+        this.updateAtricleStore(article, 1);
+      }
+     
     },
 
     onButtonGoToHome: function() {
-        var navigation = new kony.mvc.Navigation("frmMain");
-        navigation.navigate();
+      var navigation = new kony.mvc.Navigation("frmMain");
+      navigation.navigate();
     },  
 
     onButtonGoToNews: function() {
@@ -30,14 +87,14 @@ define(["NewsService"], function(newsService) {
     },
 
     onButtonGoToSearchImg: function() {
-        var navigation = new kony.mvc.Navigation("frmSearchImg");
-        navigation.navigate();
+      var navigation = new kony.mvc.Navigation("frmSearchImg");
+      navigation.navigate();
     },
 
     onButtonGoToWeather: function() {
-        var navigation = new kony.mvc.Navigation("frmWeather");
-        navigation.navigate();
+      var navigation = new kony.mvc.Navigation("frmWeather");
+      navigation.navigate();
     }
   };
 
- });
+});
