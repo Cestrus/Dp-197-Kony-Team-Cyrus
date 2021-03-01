d47c7b398df4bb146360616ddcd062811e70e305
Daryna Hunko <darynahunko@gmail.com>
2021-03-01 02:41:05 -0800

    Revert "Revert "done stars animation""

---
+++ modules/BackgroundStars.js
@@ -0,0 +1,39 @@
+function bgStars (wrap) {
+  var deviceInfo = kony.os.deviceInfo();
+  var heightDevice = deviceInfo.deviceHeight;
+  
+  var createStar = function( position ){
+    var star = new kony.ui.Label({
+      "id":  "label" + position,
+      "isVisible": true,
+      "top": position + 'px',
+      "text": ".",
+      "left": "100%",
+      "skin": "lbStar",
+      
+    },{});
+    return star;
+  };
+  
+  var starPosition = function() {
+    return Math.floor((Math.random() * heightDevice));
+  };
+  
+  var renderStar = function () {
+    var star = createStar( starPosition ());
+    var time = Math.random() * 2 + 0.5;
+    var timerId = "id" + time;
+    wrap.add(star);
+    animation_3.bind(this, star, time)();
+    kony.timer.schedule(timerId, function(){
+      wrap.remove(star);
+      kony.timer.cancel(timerId);
+    }.bind(this), time, false);
+  };
+  
+  kony.timer.schedule("timerStars", function(){
+     renderStar();      
+      kony.timer.cancel("timerStar");
+    }.bind(this), 0.1, true);
+}
+    
